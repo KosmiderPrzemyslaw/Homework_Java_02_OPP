5c94d9d30514f6b451fe161779a4ffa9613ff4d2
Przemysław Kośmider <kosmiderprzemyslaw91@gmail.com>
2020-11-26 20:39:00 +0100

    Bank Account

--- src/main/java/day_1/BankAccount.java
+++ src/main/java/day_1/BankAccount.java
@@ -1,41 +1,37 @@
 package day_1;
 
 public class BankAccount {
-    private static int number;
-    private static double cash = 0;
+    private int number;
+    private float cash = 0;
 
-    BankAccount(int number) {
-        BankAccount.number = number;
+    public BankAccount(int number) {
+        this.number = number;
     }
 
-    public static int getNumber() {
-        return number;
+    public int getNumber() {
+        return this.number;
     }
 
-    public static double getCash() {
-        return cash;
+    public float getCash() {
+        return this.cash;
     }
 
-    public static void depositCash(double amount) {
+    void depositCash(double amount) {
         if (amount > 0) {
-            double cash = getCash();
-            cash += amount;
+            this.cash += amount;
         }
     }
 
-    public static double withdrawCash(double amount) {
-        double cash = getCash();
-        if (cash > amount) {
-            return cash -= amount;
-        } else {
-            System.out.println("No found on bank account");
-        }
-        return cash;
+    public double withdrawCash(double amount) {
+
+        if (amount > 0 && amount <= cash) {
+            this.cash -= amount;
+            return cash;
+        } else return cash;
     }
 
-    public static String printInfo() {
+    public String printInfo() {
 
-        return "Count number: " + getNumber() +
-                "Available founds: " + getCash();
+        return String.format("Numer konta: %s, stan konta: %s", this.number, this.cash);
     }
 }
--- src/main/java/day_1/Main.java
+++ src/main/java/day_1/Main.java
@@ -3,10 +3,12 @@ package day_1;
 public class Main {
     public static void main(String[] args) {
         BankAccount bankAccount = new BankAccount(1);
-        BankAccount.depositCash(200.232);
-        BankAccount.printInfo();
-        BankAccount.withdrawCash(100);
-        BankAccount.printInfo();
+        bankAccount.depositCash(100);
+        bankAccount.withdrawCash(2);
+        bankAccount.printInfo();
+        System.out.println(bankAccount.getCash());
+        System.out.println(bankAccount.getNumber());
+        System.out.println(bankAccount.printInfo());
 
     }
 }
